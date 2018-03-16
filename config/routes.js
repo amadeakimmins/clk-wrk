f9dbd57a969e5e38baa27d3c61c8d11f536ce733
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-16 11:36:33 +0000

    added routes

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 
+router.route('/events')
+  .get(events.index);
+  // .post(secureRoute, events.create);
+router.route('/events/:id')
+  .get(events.show);
+  // .put(secureRoute, events.update)
+  // .delete(secureRoute, events.delete)
 
+  // authentication
+router.route('/register')
+  .post(auth.register);
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
