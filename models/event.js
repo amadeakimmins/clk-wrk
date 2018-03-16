95aa422095fa0f427a4ac0ea5ae1b44e1d90cc5a
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-16 11:32:29 +0000

    added models

---
+++ models/event.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: Number, required: true },
+  description: { type: String, required: true },
+  ticketLink: { type: String },
+  ticketPrices: { type: Number },
+  image: { type: String, required: true },
+});
+
+eventSchema.set('toJSON', {
+  getters: true,
+  virtuals: true,
+  transform(obj, json) {
+    delete json._id;
+    delete json.__v;
+  }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  fullname: { type: String, required: 'fullname required' },
+  username: { type: String, required: 'username required' },
+  email: { type: String, required: 'A valid email address is required', uniqueness: true },
+  password: { type: String, required: 'password required' },
+  admin: { true }
+}, {toObject: { virtuals: true }});
+
+
+// // Favourites from final project
+// userSchema
+//   .virtual('favorites', {
+//     ref: 'Brand',
+//     localField: '_id',
+//     foreignField: 'favorites'
+//   });
+
+userSchema.set('toJSON', {
+  getters: true,
+  virtuals: true,
+  transform(obj, json) {
+    delete json._id;
+    delete json.__v;
+    delete json.password;
+    delete json.passwordConfirmation;
+  }
+});
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(!this._passwordConfirmation || this._passwordConfirmation !== this.password) {
+    this.invalidate('passwordConfirmation', 'Passwords do not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
